f052bc8c9c569c5fb5eebb02c35d50f587008d11
DESKTOP-5LSDE5P\Miwtoo-NB <miwyugioh@gmail.com>
2017-10-11 23:17:30 +0700

    fix function

--- book94.c
+++ book94.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdio_ext.h>
 
-float sum_price(float,int);
+typedef struct{
+  char name[21];
+  float price;
+  int qty;
+}orders;
+
+float sum_price(orders[],int);
 
 int main() {
 
-  typedef struct{
-    char name[21];
-    float price;
-    int qty;
-  }orders;
+
 
   int n,i;
-  float total = 0;
 
   do {
     printf("How many orders (1-5) : ");
@@ -39,16 +40,20 @@ int main() {
   printf("-----------------------------------------------------------------\n");
 
   for (i = 0; i < n; i++) {
-    printf("%-5d%-30s%-10.2f%-5d%15.2f\n",i+1,order[i].name,order[i].price,order[i].qty,sum_price(order[i].price,order[i].qty));
-    total += sum_price(order[i].price,order[i].qty);
+    printf("%-5d%-30s%-10.2f%-5d%15.2f\n",i+1,order[i].name,order[i].price,order[i].qty,order[i].price*order[i].qty);
   }
 
   printf("-----------------------------------------------------------------\n");
-  printf("%50s%15.2f\n", "Total = ",total);
+  printf("%50s%15.2f\n", "Total = ",sum_price(order,n));
 
   return 0;
 }
 
-float sum_price(float price, int n){
-  return price*n;
+float sum_price(orders order[],int n){
+  float sum = 0.0;
+  int i;
+  for (i = 0; i < n; i++) {
+    sum += (order[i].price*order[i].qty);
+  }
+  return sum;
 }
